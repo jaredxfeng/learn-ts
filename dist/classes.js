7bcaf6510129f734316bc21fc767c6df44200fdd
jaredxfeng <92383408+jaredxfeng@users.noreply.github.com>
2024-08-28 13:31:47 -0600

    classes done

--- dist/classes.js
+++ dist/classes.js
@@ -1,16 +1,27 @@
 "use strict";
-class Pizza {
+// abstract class implements the interface with an abstract method
+class MenuItem {
     // adding access modifiers before the arguments do two things
     //   1. add the args as properties; 2. restrict corresponding access 
     constructor(title, price) {
         this.title = title;
         this.price = price;
+        // this.title = title; 
+        // this.price = price;
+    }
+    // getter method
+    get details() {
+        return `${this.title} - $${this.price}`;
+    }
+}
+// class inheritance
+class Pizza extends MenuItem {
+    constructor(title, price) {
+        super(title, price);
         // private title: string;
         // private price: number;
         this.base = 'classic';
         this.toppings = [];
-        // this.title = title; 
-        // this.price = price;
     }
     addTopping(topping) {
         this.toppings.push(topping);
@@ -18,12 +29,40 @@ class Pizza {
     removeTopping(topping) {
         this.toppings.filter((t) => t !== topping);
     }
-    selectBase(b) {
-        this.base = b;
+    selectBase(base) {
+        this.base = base;
+    }
+    format() {
+        let formatted = this.details + '\n';
+        // base
+        formatted += `Pizza on a ${this.base} base`;
+        // toppings
+        if (!this.toppings.length) {
+            formatted += ` with no toppings`;
+        }
+        else {
+            formatted += ` with ${this.toppings.join(', ')} toppings.`;
+        }
+        return formatted;
     }
 }
-const pizza = new Pizza('mario special', 15);
-pizza.selectBase('garlic');
-pizza.addTopping('mushrooms');
-pizza.addTopping('olives');
-console.log(pizza);
+const pizzaOne = new Pizza('mario special', 15); // type inference for class objects 
+const pizzaTwo = new Pizza('luigi special', 10);
+pizzaOne.addTopping(`pineapples`);
+pizzaOne.addTopping(`pineapples`);
+pizzaOne.addTopping(`pineapples`);
+pizzaOne.addTopping(`pineapples`);
+// use classes as types for function arguments
+function addMushroomsToPizzas(pizzas) {
+    for (const pizza of pizzas) {
+        pizza.addTopping("mushrooms");
+    }
+}
+// addMushroomsToPizzas([pizzaOne, pizzaTwo])
+function printMenuItem(item) {
+    console.log(item.details);
+}
+function printFormatted_(val) {
+    console.log(val.format());
+}
+printFormatted_(pizzaOne);
--- src/classes.ts
+++ src/classes.ts
@@ -1,35 +1,90 @@
 type Base = 'classic' |  'thick' | 'thin' | 'garlic';
 
-class Pizza {
+interface HasFormatter {
+    format(): string;
+}
+
+// abstract class implements the interface with an abstract method
+abstract class MenuItem implements HasFormatter {
+    // adding access modifiers before the arguments do two things
+    //   1. add the args as properties; 2. restrict corresponding access 
+    constructor(protected title: string, protected price: number) {
+        // this.title = title; 
+        // this.price = price;
+    } 
+
+    // getter method
+    get details(): string {
+        return `${this.title} - $${this.price}`;
+    }
+
+    // abstract methods must not be implemented inside the abstract class
+    // but they must be implemented in subclasses
+    // also, this is a class implementation of the respective property in the interface
+    abstract format(): string
+}
+
+// class inheritance
+class Pizza extends MenuItem {
     // private title: string;
     // private price: number;
     private base: Base = 'classic';
     private toppings: string[] = [];
 
-    // adding access modifiers before the arguments do two things
-    //   1. add the args as properties; 2. restrict corresponding access 
-    constructor(private title: string, private price: number) {
-        // this.title = title; 
-        // this.price = price;
+    constructor(title: string, price: number) {
+        super(title, price);
     }
 
     addTopping(topping: string): void {
-        this.toppings.push(topping)
+        this.toppings.push(topping);
     }
 
     removeTopping(topping: string): void {
-        this.toppings.filter((t) => t !== topping)
+        this.toppings.filter((t) => t !== topping);
     }
 
-    selectBase(b: Base): void {
-        this.base = b
+    selectBase(base: Base): void {
+        this.base = base;
+    }
+
+    format(): string {
+        let formatted = this.details + '\n';
+        
+        // base
+        formatted += `Pizza on a ${this.base} base`
+        
+        // toppings
+        if (!this.toppings.length) {
+            formatted += ` with no toppings`
+        } else {
+            formatted += ` with ${this.toppings.join(', ')} toppings.`
+        }
+        return formatted;
     }
 }
 
-const pizza = new Pizza('mario special', 15)
+const pizzaOne = new Pizza('mario special', 15); // type inference for class objects 
+const pizzaTwo: Pizza = new Pizza('luigi special', 10);
 
-pizza.selectBase('garlic')
-pizza.addTopping('mushrooms')
-pizza.addTopping('olives')
+pizzaOne.addTopping(`pineapples`)
+pizzaOne.addTopping(`pineapples`)
+pizzaOne.addTopping(`pineapples`)
+pizzaOne.addTopping(`pineapples`)
 
-console.log(pizza)
+// use classes as types for function arguments
+function addMushroomsToPizzas(pizzas: Pizza[]): void {
+    for (const pizza of pizzas) {
+        pizza.addTopping("mushrooms");
+    }
+}
+// addMushroomsToPizzas([pizzaOne, pizzaTwo])
+
+function printMenuItem(item: MenuItem): void {
+    console.log(item.details);
+}
+
+function printFormatted_(val: HasFormatter): void {
+    console.log(val.format());
+}
+
+printFormatted_(pizzaOne);
